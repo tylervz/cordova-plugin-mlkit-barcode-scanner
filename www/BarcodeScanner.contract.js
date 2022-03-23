9b9406f9ea876a62cee3a99ece6041c3cdf3c496
Tyler VanZanten <vanzanten.tyler@gmail.com>
2022-03-23 14:09:37 -0400

    Store build directory in repository so the plugin can be installed from GitHub

---
+++ www/BarcodeScanner.contract.js
@@ -0,0 +1,10 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var BarcodeScanner_plugin = require('./BarcodeScanner.plugin.js');
+
+
+
+exports.MLKitBarcodeScanner = BarcodeScanner_plugin.MLKitBarcodeScanner;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQmFyY29kZVNjYW5uZXIuY29udHJhY3QuanMiLCJzb3VyY2VzIjpbXSwic291cmNlc0NvbnRlbnQiOltdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7OzsifQ==
