4f42836d007d9f5c20a2c81b82cfe79ef31e7491
Tyler VanZanten <vanzanten.tyler@gmail.com>
2022-03-28 13:14:16 -0400

    upgrade Android to mlkit 17.0.2 and iOS to mlkit 2.6.0

--- src/android/src/CaptureActivity.java
+++ src/android/src/CaptureActivity.java
@@ -45,10 +45,10 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.common.util.concurrent.ListenableFuture;
-import com.google.mlkit.vision.barcode.Barcode;
 import com.google.mlkit.vision.barcode.BarcodeScanner;
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
 import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.barcode.common.Barcode;
 import com.google.mlkit.vision.common.InputImage;
 import com.mobisys.cordova.plugins.mlkit.barcode.scanner.utils.BitmapUtils;
 
